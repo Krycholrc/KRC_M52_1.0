ae16897b892278cb3fe7eb44cdae579169260f4a
GitHub git update Action <action@github.com>
2024-05-02 17:28:30 +0000

    Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_KRC_M52_p-p.h
+++ generated/controllers/generated/signature_KRC_M52_p-p.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3233739034
-#define TS_SIGNATURE "rusEFI default.2024.04.30.KRC_M52_p-p.3233739034"
+#define SIGNATURE_HASH 1437885057
+#define TS_SIGNATURE "rusEFI default.2024.05.02.KRC_M52_p-p.1437885057"
