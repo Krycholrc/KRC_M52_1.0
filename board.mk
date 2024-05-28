e44a7c9e5d81a853c2a2811005ac5b65e0fa3396
Krycholrc <78201348+Krycholrc@users.noreply.github.com>
2024-05-28 23:10:56 +0200

    Update board.mk

--- board.mk
+++ board.mk
@@ -10,4 +10,9 @@ include $(BOARD_DIR)/meta-info.env
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
 
+# assign critical LED to a non-existent pin if you do not have it on your board
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::Unassigned
 
+# Enable specific features for the board
+DDEFS += -DSTM32_ADC_USE_ADC12=TRUE
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE
