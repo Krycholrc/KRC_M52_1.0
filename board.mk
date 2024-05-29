6ba10a7316cd40f80be78e8e21312b5c8041abd6
Krycholrc <78201348+Krycholrc@users.noreply.github.com>
2024-05-29 08:54:17 +0200

    Update board.mk

--- board.mk
+++ board.mk
@@ -7,6 +7,11 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
+# Override DEFAULT_ENGINE_TYPE
+DDEFS += -DSTM32F407xx
+DDEFS += -DFIRMWARE_ID=\"KRC_M52\"
+DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
+
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
 
