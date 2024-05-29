825d9d496641ff46b4acef999ba68627d19e2ace
Krycholrc <78201348+Krycholrc@users.noreply.github.com>
2024-05-29 09:00:23 +0200

    Update board.mk

--- board.mk
+++ board.mk
@@ -2,7 +2,7 @@
 BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 
 # defines SHORT_BOARD_NAME
-include $(BOARD_DIR)/meta-info.env
+SHORT_BOARD_NAME=KRC_M52
 
 # Override DEFAULT_ENGINE_TYPE
 DDEFS += -DSTM32F407xx
@@ -12,9 +12,9 @@ DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
 
-# assign critical LED to a non-existent pin if you do not have it on your board
+# Not use critical LED
 DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::Unassigned
 
-# Enable specific features for the board
+# Knock
 DDEFS += -DSTM32_ADC_USE_ADC2=TRUE
 DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE
