ef26c1448e2c51de5e7479a9831182c4c02ea31c
KRCautomotive <krycholrc@tlen.pl>
2024-06-05 11:49:44 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -40,10 +40,8 @@ void setBoardDefaultConfiguration() {
 	engineConfiguration->tps1_1AdcChannel = EFI_ADC_2;
 	engineConfiguration->vbattAdcChannel = EFI_ADC_4;
 	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
-	engineConfiguration->baroSensor.hwChannel = EFI_ADC_5;
 
 	engineConfiguration->map.sensor.type = MT_MPXH6400;
-	engineConfiguration->baroSensor.type = MT_MPXH6400;
 
 	//Trigger 
 	engineConfiguration->triggerInputPins[0] = Gpio::D3;
