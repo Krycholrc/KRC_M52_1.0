303b34d1ab57029772e5ea527758d7435625a3b8
Krycholrc <78201348+Krycholrc@users.noreply.github.com>
2024-06-13 13:05:11 +0200

    Update knock_config.h

--- knock_config.h
+++ knock_config.h
@@ -12,9 +12,9 @@
 #define KNOCK_PIN_CH1 Gpio::C5
 
 // knock 2 - pin PB0
-//#define KNOCK_HAS_CH2 true
-//#define KNOCK_ADC_CH2 ADC_CHANNEL_IN8
-//#define KNOCK_PIN_CH2 Gpio::B0
+#define KNOCK_HAS_CH2 true
+#define KNOCK_ADC_CH2 ADC_CHANNEL_IN8
+#define KNOCK_PIN_CH2 Gpio::B0
 
 // Sample rate & time - depends on the exact MCU
 #define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
