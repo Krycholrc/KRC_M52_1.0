8642b23453a1432d1d7d4e63824fb13c0ee6e45a
Krycholrc <78201348+Krycholrc@users.noreply.github.com>
2024-07-30 14:28:23 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -14,30 +14,34 @@ void setBoardDefaultConfiguration() {
 
 	//Injector output
 	engineConfiguration->injectionPinMode = OM_DEFAULT;
-	engineConfiguration->injectionPins[0] = Gpio::C6;
-	engineConfiguration->injectionPins[1] = Gpio::D8;
-	engineConfiguration->injectionPins[2] = Gpio::D14;
-	engineConfiguration->injectionPins[3] = Gpio::D12;
-	engineConfiguration->injectionPins[4] = Gpio::D15;
+	engineConfiguration->injectionPins[0] = Gpio::D8;
+	engineConfiguration->injectionPins[1] = Gpio::D9;
+	engineConfiguration->injectionPins[2] = Gpio::D10;
+	engineConfiguration->injectionPins[3] = Gpio::D11;
+	engineConfiguration->injectionPins[4] = Gpio::D12;
 	engineConfiguration->injectionPins[5] = Gpio::D13;
+	engineConfiguration->injectionPins[6] = Gpio::D14;
+	engineConfiguration->injectionPins[7] = Gpio::D15;
 
         //Ignition output
 	engineConfiguration->ignitionPinMode = OM_DEFAULT;
-	engineConfiguration->ignitionPins[0] = Gpio::E2;
-	engineConfiguration->ignitionPins[1] = Gpio::E3;
-	engineConfiguration->ignitionPins[2] = Gpio::C13;
-	engineConfiguration->ignitionPins[3] = Gpio::E6;
+	engineConfiguration->ignitionPins[0] = Gpio::E0;
+	engineConfiguration->ignitionPins[1] = Gpio::E1;
+	engineConfiguration->ignitionPins[2] = Gpio::E2;
+	engineConfiguration->ignitionPins[3] = Gpio::E3;
 	engineConfiguration->ignitionPins[4] = Gpio::E4;
 	engineConfiguration->ignitionPins[5] = Gpio::E5;
+	engineConfiguration->ignitionPins[6] = Gpio::E6;
+	engineConfiguration->ignitionPins[7] = Gpio::E7;
 
 	//Digital out
-	engineConfiguration->fuelPumpPin = Gpio::D6;
+	engineConfiguration->fuelPumpPin = Gpio::E11;
 	engineConfiguration->tachOutputPin = Gpio::D7;
 
 	//Analog
 	engineConfiguration->iat.adcChannel = EFI_ADC_0;
 	engineConfiguration->clt.adcChannel = EFI_ADC_1;
-	engineConfiguration->tps1_1AdcChannel = EFI_ADC_2;
+	engineConfiguration->tps1_1AdcChannel = EFI_ADC_6;
 	engineConfiguration->vbattAdcChannel = EFI_ADC_4;
 	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
 	engineConfiguration->baroSensor.hwChannel = EFI_ADC_5;
@@ -50,8 +54,8 @@ void setBoardDefaultConfiguration() {
 	engineConfiguration->camInputs[0] = Gpio::D4;
 
 	//Bias resistor
-	engineConfiguration->clt.config.bias_resistor = 2490;
-	engineConfiguration->iat.config.bias_resistor = 2490;
+	engineConfiguration->clt.config.bias_resistor = 2200;
+	engineConfiguration->iat.config.bias_resistor = 2200;
 
 	//Analog calc
         engineConfiguration->analogInputDividerCoefficient = 1.52f;
@@ -77,8 +81,8 @@ void setBoardDefaultConfiguration() {
 	engineConfiguration->canBaudRate = B500KBPS;
 
 	//Engine configuration
-	engineConfiguration->cylindersCount = 6;
-	engineConfiguration->firingOrder = FO_1_5_3_6_2_4;
+	engineConfiguration->cylindersCount = 8;
+	engineConfiguration->firingOrder = FO_1_5_4_8_6_3_7_2;
 	engineConfiguration->ignitionMode = IM_INDIVIDUAL_COILS;
 	engineConfiguration->crankingInjectionMode = IM_SEQUENTIAL;
 	engineConfiguration->injectionMode = IM_SEQUENTIAL;
